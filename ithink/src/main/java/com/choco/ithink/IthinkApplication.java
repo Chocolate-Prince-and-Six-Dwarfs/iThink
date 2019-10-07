61455bc4858210fb992b4ce25ac6d781f9830f64
Kowaine <1228775831@qq.com>
2019-10-07 17:30:00 +0800

    初始化项目环境

---
+++ ithink/src/main/java/com/choco/ithink/IthinkApplication.java
@@ -0,0 +1,13 @@
+package com.choco.ithink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IthinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IthinkApplication.class, args);
+    }
+
+}
