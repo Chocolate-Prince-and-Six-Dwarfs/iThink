9d842ffef6621d2fd8cf50493ee76a5e433f9fc8
FantaPi <2564305544@qq.com>
2019-10-29 21:38:35 +0800

    创意推荐模块

--- ithink/src/main/resources/static/layuiJs/index.js
+++ ithink/src/main/resources/static/layuiJs/index.js
@@ -5,25 +5,10 @@ layui.define(['laypage','layer', 'form'], function(exports){
 
     layer.msg('Welcome to iThink!');
 
-    laypage.render({
-        elem: 'cutPage'
-        ,count: 50
-        ,theme: '#1E9FFF'
-        ,jump:function (obj,first) {
-            console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
-            console.log(obj.limit); //得到每页显示的条数
-            getIdeas();
-            //首次不执行
-            if(!first){
-
-            }
-        }
-    });//分页
-
-
-
+    cutPage(laypage,6);
     exports('index', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
 });
+
 layui.use('element',function () {
     var element=layui.element;//导航的hover效果、二级菜单等功能，需要依赖element模块
     //监听导航点击
@@ -32,29 +17,72 @@ layui.use('element',function () {
     });
 });
 
-function getIdeas(){
+function getIdeas(pageSize){
     var $=layui.jquery;
-
-    $.get("/example/ideaJson.json",function (data) {
-        console.log(data[0].ideaContent);
-        $("#ideaList").empty();
-        for(var i=0;i<10;i++){
-            var ideaDiv="<div style=\"padding: 15px;\" id=\"idea\""+i+">\n" +
-                "                        <fieldset class=\"layui-elem-field\">\n" +
-                "                            <legend>"+data[0].ideaTitle+"</legend>\n" +
-                "                            <div class=\"layui-field-box\">\n" +
-                "                                <p>"+data[0].ideaContent+"</p>\n" +
-                "                                <p style=\"text-align: right\"><i class=\"layui-icon\">&#xe770;"+data[0].ideaPublisher+"</i><i class=\"layui-icon\">&#xe637;"+data[0].ideaReleaseTime+"</i></p>\n" +
-                "                                <p style=\"text-align: right\">\n" +
-                "                                    <i class=\"layui-icon\">　&#xe6c6;顶一个"+data[0].ideaCollectNumber+"</i>\n" +
-                "                                    <i class=\"layui-icon\">　&#xe6c5;踩一下</i>\n" +
-                "                                    <i class=\"layui-icon\">　&#xe600;记得收藏</i>\n" +
-                "                                </p>\n" +
-                "                                <p style=\"text-align: right\"><i class=\"layui-icon\">查看详情 &#xe65b;</i></p>\n" +
-                "                            </div>\n" +
-                "                        </fieldset>\n" +
-                "                    </div>";
-            $("#ideaList").append(ideaDiv);
+    $.ajax({
+        url:"idea/load",
+        type:"post",
+        dataType: "json",
+        data:{
+            number: pageSize
+        },
+        success:function (data) {
+            $("#ideaList").empty();
+            //sortLikenumInt(data);
+            for(var i in data.data){
+                var idea="<div style=\"padding: 15px;\" id=\"idea\""+data.data[i].id+">\n" +
+                    "                        <fieldset class=\"layui-elem-field\">\n" +
+                    "                            <legend>"+data.data[i].title+"</legend>\n" +
+                    "                            <div class=\"layui-field-box\">\n" +
+                    "                                <p>"+data.data[i].content+"</p>\n" +
+                    "                                <p style=\"text-align: right\"><i class=\"layui-icon\">&#xe770;"+data.data[i].publisher+"</i><i class=\"layui-icon\">&#xe637;"+data.data[i].time+"</i></p>\n" +
+                    "                                <p style=\"text-align: right\">\n" +
+                    "                                    <i class=\"layui-icon\">　&#xe6c6;顶一个"+data.data[i].like+"</i>\n" +
+                    "                                    <i class=\"layui-icon\">　&#xe6c5;踩一下</i>\n" +
+                    "                                    <i class=\"layui-icon\">　&#xe600;记得收藏</i>\n" +
+                    "                                </p>\n" +
+                    "                                <p style=\"text-align: right\"><i class=\"layui-icon\">查看详情 &#xe65b;</i></p>\n" +
+                    "                            </div>\n" +
+                    "                        </fieldset>\n" +
+                    "                    </div>";
+                $("#ideaList").append(idea);
+            }
+        },
+        error:function () {
+            console.log("读取数据失败！");
         }
     });
+}
+
+function cutPage(laypage,pageSize) {
+    getIdeas(pageSize);
+    laypage.render({
+        elem: 'cutPage'
+        ,count: 30
+        ,limit: pageSize
+        ,theme: '#1E9FFF'
+        ,jump:function (obj,first) {
+            //console.log(obj.curr); //得到当前页，以便向服务端请求对应页的数据。
+            //console.log(obj.limit); //得到每页显示的条数
+            //首次不执行
+            if(!first){
+                getIdeas(pageSize);
+            }
+        }
+    });//分页
+}
+
+function sortLikenumInt(data) {
+    var tmp;
+    for(var i=0;i<data.data.length;i++){
+        tmp=data.data;
+        for(var j=i+1;j<data.data.length;j++){
+            if(data.data[i].like<data.data[j].like){
+                tmp=data.data[i];
+                data.data[i]=data.data[j];
+                data.data[j]=tmp;
+            }
+        }
+    }
+    return data;
 }
