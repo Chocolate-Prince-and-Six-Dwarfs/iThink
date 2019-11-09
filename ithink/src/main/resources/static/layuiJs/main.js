89efa14c112f2a513b37b5976d1ed296b0827a57
FantaPi <2564305544@qq.com>
2019-11-09 13:34:30 +0800

    搜索前端实现

--- ithink/src/main/resources/static/layuiJs/main.js
+++ ithink/src/main/resources/static/layuiJs/main.js
@@ -1,10 +1,10 @@
-layui.define(['laypage','layer', 'form','jquery'], function(exports){
+layui.define(['laypage','layer','form','jquery'], function(exports){
     var layer = layui.layer
         ,form = layui.form
         ,laypage=layui.laypage
         ,$=layui.jquery;
 
-    layer.msg('Welcome to iThink!');
+    //layer.msg('Welcome to iThink!');
     viewIdea();
     cutPage(laypage,6);
     exports('main', {}); //注意，这里是模块输出的核心，模块名必须和use时的模块名一致
