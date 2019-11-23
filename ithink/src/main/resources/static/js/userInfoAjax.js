01c082b86183db07ec6ef96c8aad9299dad15555
FantaPi <2564305544@qq.com>
2019-11-23 14:23:05 +0800

    搜索结果简单展示

--- ithink/src/main/resources/static/js/userInfoAjax.js
+++
@@ -1,8 +0,0 @@
-layui.use(['element','jquery','form'],function (){
-    var element=layui.element
-        ,$=layui.jquery
-        ,form=layui.form
-        ,layer=layui.layer;
-    //layer.msg("emmmm");
-    //getUserInfo();
-});
